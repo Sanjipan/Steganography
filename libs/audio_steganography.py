57e3c3e8bd88ad8c3acf3815c2ebdc84ccfbf697
Sanjipan <sanjipan2003@gmail.com>
2022-11-24 19:04:53 +0530

    initial commit

--- libs/audio_steganography.py
+++ libs/audio_steganography.py
@@ -4,4 +4,23 @@
 # 3. Exit
 
 def audio_steganography():
-    pass
+    def Encode():
+        pass
+
+    def Decode():
+        pass
+
+    while True:
+        print("AUDIO STEGANOGRAPHY OPERATIONS\n")
+        print("1.Encode the Text message")
+        print("2.Decode the Text message")
+        print("3.Exit")
+        n = int(input("Enter Your Choice:"))
+        if n == 1:
+            Encode()
+        elif n == 2:
+            Decode()
+        elif n == 3:
+            break
+        else:
+            print("INVALID CHOICE")
