21dfd2c119d58cfa31fae6c8b516391fac4a30eb
Sanjipan <sanjipan2003@gmail.com>
2022-11-24 11:55:44 +0530

    initial commit

--- libs/image_steganography.py
+++ libs/image_steganography.py
@@ -2,3 +2,24 @@
 # 1. Encode the Text message
 # 2. Decode the Text message
 # 3. Exit
+
+def image_Stegonography():
+    def Encode():
+        pass
+
+    def Decode():
+        pass
+
+    while True:
+        print("1. Encode the Text message")
+        print("2. Decode the Text message")
+        print("3. Exit")
+        n = int(input("Enter Your Choice:"))
+        if n == 1:
+            Encode()
+        elif n == 2:
+            Decode()
+        elif n == 3:
+            break
+        else:
+            print("Invalid")
