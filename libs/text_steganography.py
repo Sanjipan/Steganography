1563f4740b7d2b78ec6a03f5c330e387f8d9e6bc
Abanteeka <acharyaabanteeka196@gmail.com>
2022-11-25 11:38:22 +0530

    Initial Commit

--- libs/text_steganography.py
+++ libs/text_steganography.py
@@ -12,8 +12,77 @@ from matplotlib import pyplot as plt
 
 def text_steganography():
     def Encode():
-        def txt_encode(text):
-            pass
+        def txt_Encoder(text):
+            l = len(text)
+            i = 0
+            add = ''
+            while i < l:
+                t = ord(text[i])
+                if 32 <= t <= 64:
+                    t1 = t + 48
+                    t2 = t1 ^ 170
+                    res = bin(t2)[2:].zfill(8)
+                    add += "0011" + res
+                else:
+                    t1 = t - 48
+                    t2 = t1 ^ 170
+                    res = bin(t2)[2:].zfill(8)
+                    add += "0110" + res
+
+                i += 1
+
+            res1 = add + "111111111111"
+            print("The string after binary conversion applying all the transformation: " + (res1))
+            length = len(res1)
+            print("Length of binary after conversion: ", length)
+            HM_SK = ""
+            ZWC = {"00": u'\u200C', "01": u'\u202C', "11": u'\u202D', "10": u'\u200E'}
+            file_1 = open("cover_text.txt", "r+")
+            name_of_file = input("\nEnter the name of thr stego file after encoding(with extention): ")
+            file3 = open(name_of_file, "w+", encoding="utf-8")
+            word_list = []
+            for Line in file_1:
+                word_list += Line.split()
+            i = 0
+            while i < len(res1):
+                s = word_list[int(i / 12)]
+                j = 0
+                x = ""
+                HM_SK = ""
+                while j < 12:
+                    x = res1[j + 1] + res1[i + j + 1]
+                    HM_SK += ZWC[x]
+                    j += 2
+                s1 = s + HM_SK
+                file3.write(s1)
+                file3.write(" ")
+                i += 12
+            t = int(len(res1) / 12)
+            while t < len(word_list):
+                file3.write(word_list[t])
+                file3.write(" ")
+                t += 1
+            file3.close()
+            file_1.close()
+            print("Stego file has successfully generated")
+        count2 = 0
+        file1 = open("cover_text.txt", "r")
+        for line in file1:
+            for word in line.split():
+                count2 = count2 + 1
+
+        file1.close()
+        bt = int(count2)
+        print("Maximum number of words that can be inserted: ", int(bt / 6))
+        text1 = input("Enter data to be encoded: ")
+        l = len(text1)
+
+        if l <= bt:
+            print(" The text  can be hidden in the cover file")
+            txt_Encoder(text1)
+        else:
+            print("String is too big please reduce string size")
+            Encode()
 
     def BinaryToDecimal(binary):
         pass
