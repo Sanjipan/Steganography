8d2c807fd9fbbe906c7078b843eed9fc03b24700
Sanjipan <sanjipan2003@gmail.com>
2022-11-26 11:47:37 +0530

    Initial Commit

--- Steganography.py
+++ Steganography.py
@@ -1,21 +1,22 @@
 import sys
+import argparse
 
 # from libs import audio_steganography
 # from libs import image_steganography
 # from libs import text_steganography
 # from libs import vedio_steganography
 
-use_text_encode = "python3 ./Steganography.py /t /e <location of file>"
-use_text_decode = "python3 ./Steganography.py /t /d <location of file>"
+use_text_encode = "python3 ./Steganography.py -t -e <location of file>"
+use_text_decode = "python3 ./Steganography.py -t -d <location of file>"
 
-use_audio_encode = "python3 ./Steganography.py /a /e <location of file>"
-use_audio_decode = "python3 ./Steganography.py /a /d <location of file>"
+use_audio_encode = "python3 ./Steganography.py -a -e <location of file>"
+use_audio_decode = "python3 ./Steganography.py -a -d <location of file>"
 
-use_vedio_encode = "python3 ./Steganography.py /v /e <location of file>"
-use_vedio_decode = "python3 ./Steganography.py /v /d <location of file>"
+use_vedio_encode = "python3 ./Steganography.py -v -e <location of file>"
+use_vedio_decode = "python3 ./Steganography.py -v -d <location of file>"
 
-use_image_encode = "python3 ./Steganography.py /i /e <location of file>"
-use_image_decode = "python3 ./Steganography.py /i /d <location of file>"
+use_image_encode = "python3 ./Steganography.py -i -e <location of file>"
+use_image_decode = "python3 ./Steganography.py -i -d <location of file>"
 
 use_help = "python3 ./Steganography.py -h -----> help"
 
@@ -42,96 +43,50 @@ def main():
     #         break
     #     else:
     #         print("Invalid Input")
-    if sys.argv[0] == "./Steganography.py":
-        if len(sys.argv) == 2:
-            if sys.argv[1] == '/h':
-                print("FORMAT : \npython3 ./Steganography.py /<File_Type> /<e(encode)/d(decode)> <location of file>")
-                print("File_Types: /t [text], /i [image] , /a [audio], /v [vedio]")
-                print("/e [Encode], /d [Decode]")
-                print("")
-                print("For Help:")
-                print("FORMAT : python3 ./Steganography.py /h")
-            else:
-                print("For Help:")
-                print("FORMAT : python3 ./Steganography.py /h")
-        elif len(sys.argv) == 4:
-            if sys.argv[1] == '/t':
-                if sys.argv[2] == '/e':
-                    pass
-                elif sys.argv[2] == '/d':
-                    pass
-                else:
-                    print("Wrong Parameter:{}".format(sys.argv[2]))
-                    print("Formats:")
-                    print(use_text_encode)
-                    print(use_text_decode)
-                    print("/e [Encode], /d [Decode]")
-                    print("/h [For Help]")
-
-            elif sys.argv[1] == '/a':
-                if sys.argv[2] == '/e':
-                    pass
-                elif sys.argv[2] == '/d':
-                    pass
-                else:
-                    print("Wrong Parameter:{}".format(sys.argv[2]))
-                    print("Formats:")
-                    print(use_audio_encode)
-                    print(use_audio_decode)
-                    print("/e [Encode], /d [Decode]")
-                    print("/h [For Help]")
-
-            elif sys.argv[1] == '/v':
-                if sys.argv[2] == '/e':
-                    pass
-                elif sys.argv[2] == '/d':
-                    pass
-                else:
-                    print("Wrong Parameter:{}".format(sys.argv[2]))
-                    print("Formats:")
-                    print(use_vedio_encode)
-                    print(use_vedio_decode)
-                    print("/e [Encode], /d [Decode]")
-                    print("/h [For Help]")
-
-            elif sys.argv[1] == '/i':
-                if sys.argv[2] == '/e':
-                    pass
-                elif sys.argv[2] == '/d':
-                    pass
-                else:
-                    print("Wrong Parameter:{}".format(sys.argv[2]))
-                    print("Formats:")
-                    print(use_image_encode)
-                    print(use_image_decode)
-                    print("/e [Encode], /d [Decode]")
-                    print("/h [For Help]")
-            else:
-                print("Wrong Parameter:{}".format(sys.argv[1]))
-                print("FOR: text file")
-                print(use_text_encode)
-                print(use_text_decode)
-                print("FOR: audio file")
-                print(use_audio_encode)
-                print(use_audio_decode)
-                print("FOR: vedio file")
-                print(use_vedio_encode)
-                print(use_vedio_decode)
-                print("FOR: image file")
-                print(use_image_encode)
-                print(use_image_decode)
-
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-a', '--audio', action='store_true', help="For audio file")
+    parser.add_argument('-t', '--text', action='store_true', help="For text file")
+    parser.add_argument('-v', '--vedio', action='store_true', help="For vedio file")
+    parser.add_argument('-i', '--image', action='store_true', help="For image file")
+    parser.add_argument('-e', '--encode', action='store_true', help="For encoding")
+    parser.add_argument('-d', '--decode', action='store_true', help="For decoding")
+    parser.add_argument('filename')
+    args = parser.parse_args()
+    c = 0
+    t = True
+    if args.filename:
+        if args.audio and args.encode and t:
+            print("Encode audio")
+            t = False
+        elif args.audio and args.decode and t:
+            print("Decode audio")
+            t = False
+        elif args.text and args.encode and t:
+            print("Encode text")
+            t = False
+        elif args.text and args.decode and t:
+            print("Decode text")
+            t = False
+        elif args.vedio and args.encode and t:
+            print("Encode vedio")
+            t = False
+        elif args.vedio and args.decode and t:
+            print("Decode vedio")
+            t = False
+        elif args.image and args.encode and t:
+            print("Encode image")
+            t = False
+        elif args.image and args.decode and t:
+            print("Decode image")
+            t = False
         else:
-            print("Wrong Input of parameters")
-            print("For Help:")
-            print("FORMAT : python3 ./Steganography.py /h")
+            c = 1
     else:
-        print("FORMAT : \npython3 ./Steganography.py /<File_Type> /<e(encode)/d(decode)> <location of file>")
-        print("File_Types: /t [text], /i [image] , /a [audio], /v [vedio]")
-        print("/e [Encode], /d [Decode]")
-        print("")
-        print("For Help:")
-        print("FORMAT : python3 ./Steganography.py /h")
+        parser.print_help()
+        sys.exit(0)
+    if c == 1:
+        parser.print_help()
+        sys.exit()
 
 
 if __name__ == "__main__":
