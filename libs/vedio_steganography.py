3bd594d9f340f4fda6f075e751d82c2444dc495c
Sanjipan <sanjipan2003@gmail.com>
2022-11-28 12:34:24 +0530

    Initial Commit

--- libs/vedio_steganography.py
+++ libs/vedio_steganography.py
@@ -3,15 +3,65 @@
 # 2. Decode the Text message
 # 3. Exit
 import cv2
+import numpy as np
 
 
 def video_steganography(file):
 
-    def encryption(data):
-        return data
+    def PRGA(s, num):
+        i = 0
+        j = 0
+        key = []
+        while num > 0:
+            num = num - 1
+            i = (i+1) % 256
+            j = (j+s[i]) % 256
+            s[i], s[j] = s[j], s[i]
+            K = s[(s[i]+s[j]) % 256]
+            key.append(key)
+        return key
 
-    def msgtobinary(data):
-        return data
+    def KSA(key):
+        key_length = len(key)
+        S = list(range(256))
+        j = 0
+        for i in range(256):
+            j = (j+S[i]+key[i % key_length]) % 256
+            S[i], S[j] = S[j], S[i]
+        return S
+
+    def preparing_key_array(s):
+        return [ord(c) for c in s]
+
+    def encryption(plaintext):
+        key = input("Enter the Key:")
+        key = preparing_key_array(key)
+
+        S = KSA(key)
+
+        keystream = np.arry(PRGA(S, len(plaintext)))
+        plaintext = np.array([ord(i)] for i in plaintext)
+
+        cipher = keystream^plaintext
+        ctext = ''
+        for c in cipher:
+            ctext = ctext + chr(c)
+        return ctext
+
+    def msgtobinary(msg):
+        if type(msg) == str:
+            result = ''.join([format(ord(i), "08b") for i in msg])
+
+        elif type(msg) == bytes or type(msg) == np.ndarray:
+            result = [format(i, "08b") for i in msg]
+
+        elif type(msg) == int or type(msg) == np.uint8:
+            result = format(msg, "08b")
+
+        else:
+            raise TypeError("Input type is not supported in this function")
+
+        return result
 
     def embed(frame):
         data = input("Enter the data to be Encoded in Video :")
@@ -22,6 +72,23 @@ def video_steganography(file):
         data = data + '*^*^*'
 
         binary_data = msgtobinary(data)
+        length_data = len(binary_data)
+
+        index_data = 0
+        for i in frame:
+            for pixel in i:
+                r, g, b = msgtobinary(pixel)
+                if index_data < length_data:
+                    pixel[0] = int(r[:-1] + binary_data[index_data], 2)
+                    index_data = index_data + 1
+                if index_data < length_data:
+                    pixel[1] = int(g[:-1] + binary_data[index_data], 2)
+                    index_data = index_data + 1
+                if index_data < length_data:
+                    pixel[2] = int(b[:-1] + binary_data[index_data], 2)
+                    index_data = index_data + 1
+                if index_data >= length_data:
+                    break
         return frame
 
     def Encode():
@@ -51,13 +118,9 @@ def video_steganography(file):
                 break
             if frame_number == n:
                 change_frame_with = embed(frame)
-                frame_ = change_frame_with
                 frame = change_frame_with
             out.write(frame)
         print("Encoded the data successfully in the video file")
-        return frame_
-
-
 
     def Decode():
         pass
